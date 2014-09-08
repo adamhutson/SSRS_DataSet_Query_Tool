2bb1b7ac19f677d208c177c41d4d2641a32c1475
Adam Hutson <adam.hutson@yahoo.com>
2014-09-08 11:56:22 -0500

    initial creation

---
+++ SSRS_DataSet_Query_Tool/frmSSRSDataSetQueryTool.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace SSRS_DataSet_Query_Tool
+{
+    partial class frmSSRSDataSetQueryTool
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFolders = new System.Windows.Forms.TextBox();
+            this.btn_GetFolder = new System.Windows.Forms.Button();
+            this.fbdReportsFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.dgvResults = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtFolders
+            // 
+            this.txtFolders.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFolders.Location = new System.Drawing.Point(12, 15);
+            this.txtFolders.Name = "txtFolders";
+            this.txtFolders.ReadOnly = true;
+            this.txtFolders.Size = new System.Drawing.Size(730, 20);
+            this.txtFolders.TabIndex = 1;
+            this.txtFolders.Text = "Select a folder of SSRS files -->";
+            // 
+            // btn_GetFolder
+            // 
+            this.btn_GetFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_GetFolder.Location = new System.Drawing.Point(748, 13);
+            this.btn_GetFolder.Name = "btn_GetFolder";
+            this.btn_GetFolder.Size = new System.Drawing.Size(24, 23);
+            this.btn_GetFolder.TabIndex = 0;
+            this.btn_GetFolder.Text = "...";
+            this.btn_GetFolder.UseVisualStyleBackColor = true;
+            this.btn_GetFolder.Click += new System.EventHandler(this.btn_GetFolder_Click);
+            // 
+            // dgvResults
+            // 
+            this.dgvResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResults.Location = new System.Drawing.Point(12, 41);
+            this.dgvResults.Name = "dgvResults";
+            this.dgvResults.Size = new System.Drawing.Size(760, 510);
+            this.dgvResults.TabIndex = 2;
+            // 
+            // frmSSRSDataSetQueryTool
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.dgvResults);
+            this.Controls.Add(this.btn_GetFolder);
+            this.Controls.Add(this.txtFolders);
+            this.Name = "frmSSRSDataSetQueryTool";
+            this.Text = "SSRS DataSet Query Tool";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFolders;
+        private System.Windows.Forms.Button btn_GetFolder;
+        private System.Windows.Forms.FolderBrowserDialog fbdReportsFolder;
+        private System.Windows.Forms.DataGridView dgvResults;
+    }
+}
+
---
+++ SSRS_DataSet_Query_Tool/frmSSRSDataSetQueryTool.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+namespace SSRS_DataSet_Query_Tool
+{
+    public partial class frmSSRSDataSetQueryTool : Form
+    {
+        public frmSSRSDataSetQueryTool()
+        {
+            InitializeComponent();
+            InitializeGrid();
+        }
+
+        private const string _subfolderPath = "Subfolder Path";
+        private const string _reportName = "Report Name";
+        private const string _datasetName = "DataSet Name";
+        private const string _query = "Query";
+
+        private void InitializeGrid()
+        {
+            dgvResults.ColumnCount = 4;
+            dgvResults.Columns[0].Name = _subfolderPath;
+            dgvResults.Columns[1].Name = _reportName;
+            dgvResults.Columns[2].Name = _datasetName;
+            dgvResults.Columns[3].Name = _query;
+            dgvResults.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            dgvResults.RowHeadersVisible = false;
+            dgvResults.AllowUserToAddRows = false;
+            dgvResults.AllowUserToDeleteRows = false;
+        }
+
+        private void btn_GetFolder_Click(object sender, EventArgs e)
+        {
+            DialogResult result = fbdReportsFolder.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtFolders.Text = fbdReportsFolder.SelectedPath;
+                PopulateGrid(fbdReportsFolder.SelectedPath);
+            }
+        }
+
+        private void PopulateGrid(string selectedPath)
+        {
+            List<Report> reports = new List<Report>();
+
+            dgvResults.Rows.Clear();
+
+            reports = GetReportsFromDirectories(selectedPath);
+
+            foreach (Report report in reports)
+            {
+                report.ReportDataSet = GetReportDataSets(report);
+                AddReportToGrid(report);                
+            }
+        }
+
+        private List<Report> GetReportsFromDirectories(string selectedPath)
+        {
+            List<Report> reports = new List<Report>();
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(selectedPath);
+            FileInfo[] fileInfos = directoryInfo.GetFiles("*.rdl", SearchOption.AllDirectories);
+
+            foreach (FileInfo fileInfo in fileInfos)
+            {
+                reports.Add(new Report(selectedPath, fileInfo));
+            }
+
+            return reports;
+        }
+
+        private List<ReportDataSet> GetReportDataSets(Report report)
+        {
+            List<ReportDataSet> reportDataSets = new List<ReportDataSet>();
+            ReportDataSet reportDataSet = null;
+
+            XmlDocument xmlDocument = new XmlDocument();
+            using (StreamReader sr = new StreamReader(report.FullName))
+            {
+                xmlDocument.Load(sr);
+            }
+            XmlNode root = xmlDocument.DocumentElement;
+            XmlNodeList nodeList = root.SelectNodes("descendant::*");
+            foreach (XmlNode node in nodeList)
+            {
+                if (node.Name == "DataSets")
+                {
+                    XmlNodeList dataSetsChildList = node.ChildNodes;
+
+                    foreach (XmlNode dataSetsChildNode in dataSetsChildList)
+                    {
+                        reportDataSet = new ReportDataSet();
+
+                        if (dataSetsChildNode.Name == "DataSet")
+                        {
+                            reportDataSet.DataSetName = dataSetsChildNode.Attributes["Name"].Value;
+
+                            XmlNodeList dataSetChildList = dataSetsChildNode.ChildNodes;
+
+                            foreach (XmlNode dataSetChildNode in dataSetChildList)
+                            {
+                                if (dataSetChildNode.Name == "Query")
+                                {
+                                    XmlNodeList queryChildList = dataSetChildNode.ChildNodes;
+
+                                    foreach (XmlNode queryChildNode in queryChildList)
+                                    {
+                                        if (queryChildNode.Name == "CommandText")
+                                        {
+                                            reportDataSet.Query = queryChildNode.InnerText;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        reportDataSets.Add(reportDataSet);
+                    }
+                }
+            }
+
+            return reportDataSets;
+        }
+
+        private void AddReportToGrid(Report report)
+        {
+            DataGridViewRow row = null;
+            if (report.ReportDataSet.Count > 0)
+            {
+                foreach (ReportDataSet reportDataSet in report.ReportDataSet)
+                {
+                    row = new DataGridViewRow();
+                    row.CreateCells(dgvResults, report.Folder, report.ReportName, reportDataSet.DataSetName, reportDataSet.Query);
+                    dgvResults.Rows.Add(row);
+                }
+            }
+            else
+            {
+                row = new DataGridViewRow();
+                row.CreateCells(dgvResults, report.Folder, report.ReportName);
+                dgvResults.Rows.Add(row);
+            }
+        }
+    }
+
+    internal class Report
+    {
+        private string _selectedPath;
+        private FileInfo _fileInfo;
+        public Report(string selectedPath, FileInfo fileInfo) 
+        {
+            _selectedPath = selectedPath;
+            _fileInfo = fileInfo; 
+        }
+        public string Folder { get { return _fileInfo.DirectoryName.Replace(_selectedPath, string.Empty); } }
+        public string ReportName { get { return _fileInfo.Name; } }
+        public string FullName { get { return _fileInfo.FullName; } }
+        public List<ReportDataSet> ReportDataSet { get; set; }
+    }
+
+    internal class ReportDataSet
+    {
+        public string DataSetName { get; set; }
+        public string Query { get; set; }
+    }
+}
